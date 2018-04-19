9ad78a661aae75fc2b3c6a04fd02710173f826c4
Bartolome Jimenez Vera <bjv@blue-ocean-robotics.com>
2018-04-19 09:49:46 +0200

    Compiles and fixed clang position

--- ur_caros_example/src/ur_caros_example.cpp
+++ ur_caros_example/src/ur_caros_example.cpp
@@ -22,10 +22,11 @@ using namespace rwlibs::proximitystrategies;
 
 #define MAXTIME 10
 
-class URRobot {
+class URRobot
+{
   using Q = rw::math::Q;
 
- private:
+private:
   ros::NodeHandle nh;
   rw::models::WorkCell::Ptr wc;
   rw::models::Device::Ptr device;
@@ -34,26 +35,27 @@ class URRobot {
   Q q_home;
   // rw::invkin::JacobianIKSolver *solver;
 
- public:
-  URRobot() {
+public:
+  URRobot()
+  {
     auto packagePath = ros::package::getPath("ur_caros_example");
-    wc = rw::loaders::WorkCellLoader::Factory::load(
-        packagePath + "/WorkStation_1/WC1_Scene.wc.xml");
+    wc = rw::loaders::WorkCellLoader::Factory::load(packagePath + "/WorkStation_1/WC1_Scene.wc.xml");
     device = wc->findDevice("UR1");
     state = wc->getDefaultState();
     robot = new caros::SerialDeviceSIProxy(nh, "caros_universalrobot");
 
     // Wait for first state message, to make sure robot is ready
-    ros::topic::waitForMessage<caros_control_msgs::RobotState>(
-        "/caros_universalrobot/caros_serial_device_service_interface/"
-        "robot_state",
-        nh);
+    ros::topic::waitForMessage<caros_control_msgs::RobotState>("/caros_universalrobot/"
+                                                               "caros_serial_device_service_interface/"
+                                                               "robot_state",
+                                                               nh);
     // init q_home to its basic configuration
     // q_home = {};
     ros::spinOnce();
   }
 
-  Q getQ() {
+  Q getQ()
+  {
     // spinOnce processes one batch of messages, calling all the callbacks
     ros::spinOnce();
     Q q = robot->getQ();
@@ -61,18 +63,19 @@ class URRobot {
     return q;
   }
 
-  bool setQ(Q q) {
+  bool setQ(Q q)
+  {
     // Tell robot to move to joint config q
     float speed = 1;
-    std::cout << "dentro de setQ .>  q :" << std::endl
-              << q << std::endl
-              << std::endl;
-    if (robot->moveServoQ(q, speed)) {
+    std::cout << "dentro de setQ .>  q :" << std::endl << q << std::endl << std::endl;
+    if (robot->moveServoQ(q, speed))
+    {
       // std::cout << "If setQ :" << std::endl;
       // moveServoQ terminates before the robot is at the destination, so wait
       // until it is
       Q qCurrent;
-      do {
+      do
+      {
         // std::cout << "Current joint config:" << std::endl << robot->getQ() <<
         // std::endl << std::endl;
         ros::spinOnce();
@@ -81,44 +84,42 @@ class URRobot {
       // std::cout << "antes de device (SET Q) " << std::endl;
       device->setQ(q, state);
       return true;
-    } else
+    }
+    else
       return false;
   }
 
   //////////////////////////////////////////////////////////////////////////////
 
-  rw::math::VelocityScrew6D<double> calculateDeltaU(
-      const rw::math::Transform3D<double> &baseTtool,
-      const rw::math::Transform3D<double> &baseTtool_desired) {
+  rw::math::VelocityScrew6D<double> calculateDeltaU(const rw::math::Transform3D<double> &baseTtool,
+                                                    const rw::math::Transform3D<double> &baseTtool_desired)
+  {
     // Calculate the positional difference, dp
     // cout << "dentro calcula de delta U " << endl;
     rw::math::Vector3D<double> dp = baseTtool_desired.P() - baseTtool.P();
 
     // Calculate the rotational difference, dw
-    rw::math::EAA<double> dw(baseTtool_desired.R() *
-                             rw::math::inverse(baseTtool.R()));
+    rw::math::EAA<double> dw(baseTtool_desired.R() * rw::math::inverse(baseTtool.R()));
 
     return rw::math::VelocityScrew6D<double>(dp, dw);
   }
 
-  rw::math::Q algorithm1(const rw::models::Device::Ptr device,
-                         rw::kinematics::State state,
-                         const rw::kinematics::Frame *tool,
-                         const rw::math::Transform3D<double> baseTtool_desired,
-                         rw::math::Q q) {
+  rw::math::Q algorithm1(const rw::models::Device::Ptr device, rw::kinematics::State state,
+                         const rw::kinematics::Frame *tool, const rw::math::Transform3D<double> baseTtool_desired,
+                         rw::math::Q q)
+  {
     // We need an initial base to tool transform and the positional error at the
     // start (deltaU)
     rw::math::Transform3D<> baseTtool = device->baseTframe(tool, state);
-    rw::math::VelocityScrew6D<double> deltaU =
-        calculateDeltaU(baseTtool, baseTtool_desired);
+    rw::math::VelocityScrew6D<double> deltaU = calculateDeltaU(baseTtool, baseTtool_desired);
 
     // This epsilon is the desired tolerance on the final position.
     const double epsilon = 0.1;
 
-    while (deltaU.norm2() > epsilon) {
-      rw::math::Jacobian J = device->baseJframe(
-          tool, state);  // This line does the same as the function from
-                         // Programming Exercise 4.1
+    while (deltaU.norm2() > epsilon)
+    {
+      rw::math::Jacobian J = device->baseJframe(tool, state);  // This line does the same as the function from
+                                                               // Programming Exercise 4.1
       // We need the inverse of the jacobian. To do that, we need to access the
       // Eigen representation of the matrix. For information on Eigen, see
       // http://eigen.tuxfamily.org/.
@@ -154,7 +155,8 @@ class URRobot {
   }
   /////////////////////////////////////////////////////////////////////////////////
 
-  bool setPose(rw::math::Vector3D<double> pos, RPY<double> angles) {
+  bool setPose(rw::math::Vector3D<double> pos, RPY<double> angles)
+  {
     ros::spinOnce();
     // std::cout <<"RPY UNO: "<<angles[0]<<" "<<angles[1]<<"
     // "<<angles[2]<<std::endl;  std::cout <<"POS UNO: "<<pos[0]<<" "<<pos[1]<<"
@@ -165,7 +167,8 @@ class URRobot {
     for(int i = 0; i < frames.size(); i++){
             std::cout<<frames[i]->name<<std::endl;
     }*/
-    if (tool_frame == nullptr) {
+    if (tool_frame == nullptr)
+    {
       RW_THROW("Tool frame not found!");
     }
     rw::math::Transform3D<double> baseTtool_desired(pos, angles.toRotation3D());
@@ -180,9 +183,9 @@ class URRobot {
     // auto solution = solver.solve(baseTtool_desired,state);
     std::vector<rw::math::Q> solution = solver->solve(baseTtool_desired, state);
     for (int k = 0; k < solution.size(); k++)
-      std::cout << "solution solve inkin " << k << " -> " << solution[k]
-                << std::endl;
-    if (solution.size() > 0) setQ(solution[0]);
+      std::cout << "solution solve inkin " << k << " -> " << solution[k] << std::endl;
+    if (solution.size() > 0)
+      setQ(solution[0]);
 
     /*rw::math::Q q_algo =
         algorithm1(device, state, tool_frame, baseTtool_desired, getQ());*/
@@ -190,7 +193,8 @@ class URRobot {
     return true;
   }
 
-  bool getPose() {
+  bool getPose()
+  {
     // spinOnce processes one batch of messages, calling all the callbacks
 
     // rw::math::Pose6D<double> pose = rw::math::Pose6D::getPos();
@@ -210,16 +214,14 @@ class URRobot {
     auto endTransform = device->baseTframe(endFrame, state);
     auto rpy = RPY<double>(endTransform.R());
     auto pos = endTransform.P();
-    std::cout << "RPY UNO: " << rpy[0] << " " << rpy[1] << " " << rpy[2]
-              << std::endl;
-    std::cout << "POS UNO: " << pos[0] << " " << pos[1] << " " << pos[2]
-              << std::endl;
+    std::cout << "RPY UNO: " << rpy[0] << " " << rpy[1] << " " << rpy[2] << std::endl;
+    std::cout << "POS UNO: " << pos[0] << " " << pos[1] << " " << pos[2] << std::endl;
 
     return true;
   }
 
-  bool checkCollisions(Device::Ptr device, const State &state,
-                       const CollisionDetector &detector, const Q &q) {
+  bool checkCollisions(Device::Ptr device, const State &state, const CollisionDetector &detector, const Q &q)
+  {
     State testState;
     CollisionDetector::QueryResult data;
     bool colFrom;
@@ -227,28 +229,31 @@ class URRobot {
     testState = state;
     device->setQ(q, testState);
     colFrom = detector.inCollision(testState, &data);
-    if (colFrom) {
+    if (colFrom)
+    {
       cerr << "Configuration in collision: " << q << endl;
       cerr << "Colliding frames: " << endl;
       FramePairSet fps = data.collidingFrames;
-      for (FramePairSet::iterator it = fps.begin(); it != fps.end(); it++) {
-        cerr << (*it).first->getName() << " " << (*it).second->getName()
-             << endl;
+      for (FramePairSet::iterator it = fps.begin(); it != fps.end(); it++)
+      {
+        cerr << (*it).first->getName() << " " << (*it).second->getName() << endl;
       }
       return false;
     }
     return true;
   }
 
-  rw::math::Q nearest_neigbor(rw::math::Q q_rand,
-                              std::vector<rw::math::Q> path) {
+  rw::math::Q nearest_neigbor(rw::math::Q q_rand, std::vector<rw::math::Q> path)
+  {
     double min = 10000;
     double dist = 0;
     Q q_nearest = path[0];
-    for (int k = 0; k < path.size(); k++) {
+    for (int k = 0; k < path.size(); k++)
+    {
       Q dq = q_rand - path[k];
       dist = dq.norm2();
-      if (dist < min) {
+      if (dist < min)
+      {
         min = dist;
         q_nearest = path[k];
       }
@@ -256,11 +261,9 @@ class URRobot {
     return q_nearest;
   }
 
-
-
-  bool NewConfig(rw::math::Q q_rand, rw::math::Q q_near, rw::math::Q *q_new) {
-    CollisionDetector detector(
-        wc, ProximityStrategyFactory::makeDefaultCollisionStrategy());
+  bool NewConfig(rw::math::Q q_rand, rw::math::Q q_near, rw::math::Q &q_new)
+  {
+    CollisionDetector detector(wc, ProximityStrategyFactory::makeDefaultCollisionStrategy());
 
     Q dq = q_near - q_rand;
     double extend = 0.01;
@@ -269,20 +272,19 @@ class URRobot {
     // cuando no choca -> true
   }
 
-
-
-  bool planner_rrt(double x, double y, double z , RPY<double> rpy ){
-    
+  bool planner_rrt(double x, double y, double z, RPY<double> rpy)
+  {
     // PlannerConstraint constraint =
     //     PlannerConstraint::make(&detector, device, state);
 
     //  QSampler::Ptr sampler = QSampler::makeConstrained(
     //      QSampler::makeUniform(device), constraint.getQConstraintPtr());
     // QMetric::Ptr metric = MetricFactory::makeEuclidean<Q>();
-    
+
     // QToQPlanner::Ptr planner = RRTPlanner::makeQToQPlanner(
     //     constraint, sampler, metric, extend, RRTPlanner::RRTConnect);
 
+    // TODO Initialize this value at start (Get default_q from configuration)
     Q from(6, 0, -1.5708, 0, -1.57095, 0, -0.000153494);
     // Q to(6,1.7,0.6,-0.8,0.3,0.7,-0.5); // Very difficult for planner
     // Q to(6, 1.07129, -0.803471, -0.431695, -1.91307, -2.6387, 0.775581);
@@ -290,56 +292,59 @@ class URRobot {
     - obtener rand transf..
     */
     rw::math::Vector3D<double> pos(x, y, z);
-    //auto rpy = RPY<double>(yaw, p, r);  /// OJO al yaw , r
+    // auto rpy = RPY<double>(yaw, p, r);  /// OJO al yaw , r
 
     rw::math::Transform3D<double> baseTtool_desired(pos, rpy.toRotation3D());
     auto solver = new rw::invkin::JacobianIKSolver(device, state);
     rw::math::Q q_goal;
-    std::vector<rw::math::Q> solutions =
-        solver->solve(baseTtool_desired, state);
+    std::vector<rw::math::Q> solutions = solver->solve(baseTtool_desired, state);
     std::vector<rw::math::Q> path;
     path.push_back(from);
     rw::math::Q diferencia;
 
-    if (solutions.size() > 0) q_goal = solutions[0];
+    if (solutions.size() > 0)
+    {
+      q_goal = solutions[0];
+    }
 
-    do {
-      auto x_rand = x - 0.1 +
-                    static_cast<float>(rand()) /
-                        (static_cast<float>(RAND_MAX / (0.1 + 0.1)));
-
-      auto y_rand = y - 0.1 +
-                    static_cast<float>(rand()) /
-                        (static_cast<float>(RAND_MAX / (0.1 + 0.1)));
-      auto z_rand = z - 0.1 +
-                    static_cast<float>(rand()) /
-                        (static_cast<float>(RAND_MAX / (0.1 + 0.1)));
+    do
+    {
+      auto x_rand = x - 0.1 + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (0.1 + 0.1)));
+      auto y_rand = y - 0.1 + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (0.1 + 0.1)));
+      auto z_rand = z - 0.1 + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (0.1 + 0.1)));
 
       rw::math::Vector3D<double> pos_rand(x_rand, y_rand, z_rand);
       rw::math::Transform3D<double> baseTtool_desired_rand(pos_rand, rpy.toRotation3D());
+      // Biased solution towards goal area
       std::vector<rw::math::Q> solutions_rand = solver->solve(baseTtool_desired_rand, state);
       rw::math::Q q_rand;
-      if (solutions.size() > 0) q_rand = solutions_rand[0];
-      
-      
+
+      if (solutions.size() > 0)
+      {
+        q_rand = solutions_rand[0];
+      }
+
       Q q_near = nearest_neigbor(q_rand, path);
       Q q_new;
-      if (NewConfig(q_rand, q_near, &q_new))
+
+      if (NewConfig(q_rand, q_near, q_new))
+      {
         path.push_back(q_new);
+      }
 
-
-      diferencia = q_new - q_goal; 
+      diferencia = q_new - q_goal;
     } while (diferencia.norm2() > 0.1);
 
-    //if (!checkCollisions(device, state, detector, from)) return true;
-    //if (!checkCollisions(device, state, detector, to)) return true;
-   // cout << "Planning from " << from << " to " << to << endl;
+    // if (!checkCollisions(device, state, detector, from)) return true;
+    // if (!checkCollisions(device, state, detector, to)) return true;
+    // cout << "Planning from " << from << " to " << to << endl;
 
     // planner->query(from, to, path, MAXTIME);
 
     cout << "Path of length " << path.size() << endl;
 
-    for (QPath::iterator it = path.begin(); it < path.end(); it++) {
+    for (QPath::iterator it = path.begin(); it < path.end(); it++)
+    {
       // cout << *it << endl;
       cout << "set Q " << *it << endl;
       setQ(*it);
@@ -350,7 +355,8 @@ class URRobot {
   }
 };
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   ros::init(argc, argv, "URRobot");
   URRobot robot;
 
