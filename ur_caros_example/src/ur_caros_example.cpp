2ab523086deb50bc3f5abd1cf247644cb144890a
Noelia <nofer17@student.sdu.dk>
2018-04-25 11:26:29 +0200

    1 - Implemente inverse kinematics function. 2 - Goal region added. 3 - Check edges using algorithm 1

--- ur_caros_example/src/ur_caros_example.cpp
+++ ur_caros_example/src/ur_caros_example.cpp
@@ -62,6 +62,24 @@ class URRobot {
     device->setQ(q, state);
     return q;
   }
+  ////////////////// FUNCTION INVERSE KINEMATICS ///////////////////////
+
+  rw::math::Q inverseKinematics(rw::math::Vector3D<double> pos,
+                                RPY<double> rpy) {
+    rw::math::Q q_inverse;
+    rw::math::Transform3D<double> baseTtool_desired(pos, rpy.toRotation3D());
+    auto solver = new rw::invkin::JacobianIKSolver(device, state);
+    std::vector<rw::math::Q> solutions =
+        solver->solve(baseTtool_desired, state);
+    if (solutions.size() < 1)
+      cout << "El solver no ha encontrado solucion " << endl;
+    if (solutions.size() > 0) {
+      q_inverse = solutions[0];
+      cout << "Q inverse " << q_inverse << endl;
+    }
+    return q_inverse;
+  }
+  ////////////////////////////////////////////////////////////////////////
 
   bool setQ(Q q) {
     // Tell robot to move to joint config q
@@ -92,7 +110,8 @@ class URRobot {
     // std::cout <<"RPY UNO: "<<angles[0]<<" "<<angles[1]<<"
     // "<<angles[2]<<std::endl;  std::cout <<"POS UNO: "<<pos[0]<<" "<<pos[1]<<"
     // "<<pos[2]<<std::endl;
-    rw::kinematics::Frame *tool_frame = device->getEnd();// = wc->findFrame("PG70.TCP");
+    rw::kinematics::Frame *tool_frame =
+        device->getEnd();  // = wc->findFrame("PG70.TCP");
     /*std::vector<rw::kinematics::Frame*> frames = wc->getFrames();
 
     for(int i = 0; i < frames.size(); i++){
@@ -101,12 +120,17 @@ class URRobot {
     if (tool_frame == nullptr) {
       RW_THROW("Tool frame not found!");
     }
+    rw::math::Q solution = inverseKinematics(pos, angles);
+    setQ(solution);
+    /*
     rw::math::Transform3D<double> baseTtool_desired(pos, angles.toRotation3D());
     // The inverse kinematics algorithm needs to know about the device, the tool
     // frame and the desired pose. These parameters are const since they are not
     // changed by inverse kinematics We pass the state and the configuration, q,
     // as value so we have copies that we can change as we want during the
     // inverse kinematics.
+
+
 
     auto solver = new rw::invkin::JacobianIKSolver(device, state);
     // auto solver = rw::invkin::JacobianIKSolver(device, state);
@@ -117,8 +141,8 @@ class URRobot {
                 << std::endl;
     }
     if (solution.size() > 0) {
-      setQ(solution[0]);
-    }
+
+    }*/
 
     /*rw::math::Q q_algo =
         algorithm1(device, state, tool_frame, baseTtool_desired, getQ());*/
@@ -141,9 +165,9 @@ class URRobot {
     std::cout << "name " << std::endl << name << std::endl << std::endl;*/
 
     ros::spinOnce();
-    auto endFrame = device->getEnd();//wc->findFrame("PG70.TCP");
+    auto endFrame = device->getEnd();  // wc->findFrame("PG70.TCP");
     auto endTransform = device->baseTframe(endFrame, state);
-   // cout << "end transform . R " <<endTransform.R()<< endl;
+    // cout << "end transform . R " <<endTransform.R()<< endl;
     auto rpy = RPY<double>(endTransform.R());
     auto pos = endTransform.P();
     std::cout << "POS: " << pos[0] << " " << pos[1] << " " << pos[2]
@@ -156,24 +180,35 @@ class URRobot {
   }
 
   bool checkCollisions(Device::Ptr device, const State &state,
-                       const CollisionDetector &detector, const Q &q) {
+                       const CollisionDetector &detector, const Q &q_new,
+                       const Q &q_near, double extend) {
     State testState;
     CollisionDetector::QueryResult data;
     bool colFrom;
 
-    testState = state;
-    device->setQ(q, testState);
-    colFrom = detector.inCollision(testState, &data);
-    if (colFrom) {
-      cerr << "Configuration in collision: " << q << endl;
-      cerr << "Colliding frames: " << endl;
-      FramePairSet fps = data.collidingFrames;
-      for (FramePairSet::iterator it = fps.begin(); it != fps.end(); it++) {
-        cerr << (*it).first->getName() << " " << (*it).second->getName()
-             << endl;
+    // algoritmo 1 check collision // dividimos el edge en 3 y comprobamos esos tres puntos
+    double epsilon = extend/3;
+    rw::math::Q delta_q = q_new - q_near;
+    double norm = delta_q.norm2(); 
+    int n = ceil(norm/(epsilon)) - 1;
+    rw::math::Q q;
+    for (int i = 1; i<=n; i++) {
+      q = i*epsilon*(delta_q/norm) + q_near;
+      testState = state;
+      device->setQ(q, testState);
+      colFrom = detector.inCollision(testState, &data);
+      if (colFrom) {
+        cerr << "Configuration in collision: " << q << endl;
+        cerr << "Colliding frames: " << endl;
+        FramePairSet fps = data.collidingFrames;
+        for (FramePairSet::iterator it = fps.begin(); it != fps.end(); it++) {
+          cerr << (*it).first->getName() << " " << (*it).second->getName()
+               << endl;
+        }
+        return false;
       }
-      return false;
     }
+
     return true;
   }
 
@@ -200,7 +235,7 @@ class URRobot {
     Q dq = q_near - q_rand;
     double extend = 0.01;
     q_new = q_rand + extend * (dq) / (dq.norm2());
-    return checkCollisions(device, state, detector, q_new);
+    return checkCollisions(device, state, detector, q_new, q_near, extend);
     // cuando no choca -> true
   }
 
@@ -213,21 +248,28 @@ class URRobot {
 
   bool planner_rrt(double x, double y, double z, RPY<double> rpy) {
     rw::math::Vector3D<double> pos(x, y, z);
-    rw::math::Q q_goal;
-    rw::math::Transform3D<double> baseTtool_desired(pos, rpy.toRotation3D());
-    auto solver = new rw::invkin::JacobianIKSolver(device, state);
-    std::vector<rw::math::Q> solutions =
-        solver->solve(baseTtool_desired, state);
-    if (solutions.size() < 1)
-      cout << "El solver no ha encontrado solucion " << endl;
+
+    rw::math::Q q_goal = inverseKinematics(pos, rpy);
+
     std::vector<rw::math::Q> path;
     path.push_back(getQ());
     rw::math::Q diferencia;
+    rw::math::Q dif1;
+    rw::math::Q dif2;
+    rw::math::Q dif3;
+    rw::math::Q dif4;
 
-    if (solutions.size() > 0) {
-      q_goal = solutions[0];
-      cout << "Q goal " << q_goal << endl;
-    }
+    double region = 0.05;
+
+    rw::math::Vector3D<double> pos1(x + region, y + region, z);
+    rw::math::Vector3D<double> pos2(x - region, y + region, z);
+    rw::math::Vector3D<double> pos3(x - region, y - region, z);
+    rw::math::Vector3D<double> pos4(x + region, y - region, z);
+
+    rw::math::Q q1_limit = inverseKinematics(pos1, rpy);
+    rw::math::Q q2_limit = inverseKinematics(pos2, rpy);
+    rw::math::Q q3_limit = inverseKinematics(pos3, rpy);
+    rw::math::Q q4_limit = inverseKinematics(pos4, rpy);
 
     do {
       // TODO Set as constants
@@ -242,16 +284,8 @@ class URRobot {
                         (static_cast<float>(RAND_MAX / (0.001 + 0.001)));
 
       rw::math::Vector3D<double> pos_rand(x_rand, y_rand, z_rand);
-      rw::math::Transform3D<double> baseTtool_desired_rand(pos_rand,
-                                                           rpy.toRotation3D());
-      // Biased solution towards goal area
-      std::vector<rw::math::Q> solutions_rand =
-          solver->solve(baseTtool_desired_rand, state);
-      rw::math::Q q_rand;
 
-      if (solutions.size() > 0) {
-        q_rand = solutions_rand[0];
-      }
+      rw::math::Q q_rand = inverseKinematics(pos_rand, rpy);
 
       Q q_near = nearest_neigbor(q_rand, path);
       Q q_new;
@@ -261,7 +295,14 @@ class URRobot {
       }
 
       diferencia = q_new - q_goal;
-    } while (diferencia.norm2() > 0.05);
+      dif1 = q_new - q1_limit;
+      dif2 = q_new - q2_limit;
+      dif3 = q_new - q3_limit;
+      dif4 = q_new - q4_limit;
+
+    } while (diferencia.norm2() > region && dif1.norm2() > region &&
+             dif2.norm2() > region && dif3.norm2() > region &&
+             dif4.norm2() > region);
 
     cout << "Path of length " << path.size() << endl;
 
@@ -286,7 +327,8 @@ int main(int argc, char **argv) {
   std::cout << "Q inicial -> " << robot.getQ()
             << std::endl;  // para mostrar nada mas
   // std::cout << robot.getPose() << std::endl;
-  rw::math::Q q_ini(6, -0.384695, -1.08527, 1.37812, -0.309625, 1.56999, 0.0178859);
+  rw::math::Q q_ini(6, -0.384695, -1.08527, 1.37812, -0.309625, 1.56999,
+                    0.0178859);
   robot.setQ(q_ini);
 
   auto robot_transform = robot.getPose();  // Devuelve transform3D
@@ -300,7 +342,7 @@ int main(int argc, char **argv) {
 
   robot.planner_rrt(x, y, z, rpy);
 
-  // Usando UR1.TCP -> rpy 
+  // Usando UR1.TCP -> rpy
 
   cout << "New Pose Robot: " << endl;
   auto new_transform = robot.getPose();
